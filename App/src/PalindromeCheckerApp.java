e33c42532aa5d235b20078b05ff8cbcbf547f2d8
akaayu17 <kuikelaayusha7@gmail.com>
2026-02-27 16:53:30 +0530

    UC5: Stack-Based Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,34 +1,43 @@
+import java.sql.SQLOutput;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
     /**
-     * Description:
-     * This class validates a palindrome by converting * the string into a character array and comparing * characters using the two-pointer technique.
-     * At this stage, the application:
+     * Use Case 5: Stack Based Palindrome Checker
+     Description:
+     * This class validates a palindrome using a Stack
+     * data structure which follows the LIFO principle.
      *
-     Converts string to char array
-     * Uses start and end pointers
-     * Compares characters efficiently
-     * Displays the result
-     * This reduces extra memory usage.
+     * At this stage, the application:
+     * - Pushes characters into a stack
+     * - Pops them in reverse order
+     * - Compares with original sequence
+     * - Displays the result
+
+     * This maps stack behavior to reversal Logic.
      *
      * @author Aayusha Kuikel
      * @Version 4.0
      */
 
     public static void main(String[] args) {
-
-
-        String str = "madam";
-        char[] arr = str.toCharArray();
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            if (arr[start++] != arr[end--]) {
-                System.out.println(" the given string " + str + " is not palindrome ");
-                return;
-            }
+        Stack<Character> str = new Stack<>();
+        String string="aayusha";
+        for(char c: string.toCharArray()){
+            str.push(c);
         }
-        System.out.println(" The given string " + str + " is palindrome ");
+
+        for(char c: string.toCharArray()){
+            if(c!=str.pop()){
+
+                System.out.println("The string"+ str + " is not palindrome");
+
+            return;
+            }
+            System.out.println("The string " + str + " is palindrome");
+        }
+
+
     }
 }
 
