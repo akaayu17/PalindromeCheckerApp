287fe57cf6c8b106fa3a062713d6d065984a4821
akaayu17 <kuikelaayusha7@gmail.com>
2026-02-27 15:32:50 +0530

    uc1 completed

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+
+public class PalindromeCheckerApp {
+    /**
+     * Application entry point.
+     *
+     * This is a Palindrome Checker App created from Java
+     * @author Aayusha Kuikel
+     * @Version 1.0
+     */
+
+    public static void main(String[] args){
+        System.out.println(" Welcome to the Palindrome Checker Management System \n Version : 1.0 \n System initialized successfully. ");
+
+    }
+}
