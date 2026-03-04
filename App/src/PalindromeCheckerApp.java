25e1fd498c24898216fe6b53cdd1c4cfa8964960
akaayu17 <kuikelaayusha7@gmail.com>
2026-03-04 22:10:17 +0530

    Normalized Palindrome Validation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,45 +1,49 @@
 
 
+
 import java.util.*;
 
 /**
  * main class - use case palindrome app
  *
- * use case 9:Recursive Palindrome checker
+ * use case 10:Normalized Palindrome Validation
  *
  * Description:
- * this class validates the palindrome using Call Stack
+ * this class validates the palindrome after processing the input string
  *
  *  the application:
- *  intialize start and end index of the string
- *  checks the element of starting index and ending index recursively
- *  displays the result
+ *  get the input string and convert it into lowercase
+ *  remove the spaces and other characters from input string and form a normalized string
+ *  check for palindrome and display the result
  *
  *
  * @author  Aayusha Kuikel
- * @version 9.0
+ * @version 10.0
  */
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "level";
-       int start=0;
-       int end=input.length()-1;
+        String input = "A man a plan a canal panama";
+        boolean isPalindrome=true;
+        input=input.toLowerCase();
+        String normalizedString ="";
+        for(char c:input.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                normalizedString+=c;
+            }
+        }
+
+        for(int i=0;i<normalizedString.length()-1;i++){
+            if(normalizedString.charAt(i)!=normalizedString.charAt(normalizedString.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
+        }
+
 
 
         System.out.println("input: "+input);
-        System.out.println("is Palindrome? :"+isPalindrome(input,start,end));
+        System.out.println("is Palindrome? :"+isPalindrome);
 
-
-    }
-    public static boolean isPalindrome(String s, int start , int end){
-        if (start>=end){
-            return true;
-        }
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(s,start+1,end-1);
 
     }
 }
